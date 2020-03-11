16d3c6d0ad239383a9243df438916cf5db956332
Ye_Junhao <HuashiSCNU0303@hotmail.com>
2020-03-11 20:43:07 +0800

    update Homework4

---
+++ Homework4/Project_04/AlarmEvent/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+
+namespace AlarmEvent
+{
+    public delegate void ClockEventHandler(object sender, ClockEventArgs args);
+    public class ClockEventArgs
+    {
+        public DateTime alarmingTime { get; set; }
+        public DateTime tickingTime { get; set; }
+    }
+    class Clock
+    {
+        public event ClockEventHandler TickEvent;
+        public event ClockEventHandler AlarmEvent;
+        public void Tick()
+        {
+            Thread.Sleep(1000);
+            ClockEventArgs args = new ClockEventArgs()
+            {
+                tickingTime = DateTime.Now
+            };
+            TickEvent(this, args);
+        }
+        public void Alarm()
+        {
+            ClockEventArgs args = new ClockEventArgs()
+            {
+                alarmingTime = DateTime.Now
+            };
+            AlarmEvent(this, args);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            clock.TickEvent += TickListener;
+            clock.AlarmEvent += AlarmListener;
+            while(true)
+            {
+                Console.Write("你想让闹钟几秒钟后开始响：");
+                string input = Console.ReadLine();
+                int alarmingTime = 0;
+                while(true)
+                {
+                    try
+                    { 
+                        alarmingTime = Convert.ToInt32(input);
+                        break;
+                    }   
+                    catch(FormatException)
+                    {
+                        Console.WriteLine("请输入整数！");
+                        Console.Write("重新输入：");
+                        input = Console.ReadLine();
+                    }
+                }
+                for (int i = 0; i < alarmingTime; i++) 
+                {
+                    clock.Tick();
+                }
+                clock.Alarm();
+                Console.Write("还需要继续响铃吗？如果不需要，输入Stop，否则，输入任意字符：");
+                if (Console.ReadLine() == "Stop")
+                {
+                    break;
+                }
+            }          
+        }
+        static void TickListener(object sender, ClockEventArgs args)
+        {
+            Console.WriteLine("秒针摆动！当前时间：" + args.tickingTime);
+        }
+        static void AlarmListener(object sender, ClockEventArgs args)
+        {
+            Console.WriteLine("响铃啦！当前时间：" + args.alarmingTime);
+        }
+    }
+}
---
+++ Homework4/Project_04/NewLinkList/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace NewLinkList
+{
+    // 链表节点
+  public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    // 泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void Seal()
+        {
+            tail.Next = null;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            Node<T> node = Head;
+            while (node != null) 
+            {
+                action(node.Data);
+                node = node.Next;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> linkList = new GenericList<int>();
+            while (true) 
+            {
+                Console.Write("请输入数据元素，或输入Stop结束输入：");
+                string input = Console.ReadLine();
+                if (input == "Stop")
+                {
+                    linkList.Seal();
+                    break;
+                }
+                else 
+                {
+                    int data = 0;
+                    try 
+                    {
+                        data = Convert.ToInt32(input);
+                        linkList.Add(data);
+                    }
+                    catch(FormatException)
+                    {
+                        Console.WriteLine("请输入整数！");
+                    }
+                }
+            }
+            Console.Write("链表内容输出：");
+            linkList.ForEach(m => Console.Write(m + " "));
+            Console.WriteLine("");
+
+            int maxNum = linkList.Head.Data;
+            linkList.ForEach(m => { if (m > maxNum) maxNum = m; });
+            Console.WriteLine("链表最大值为：" + maxNum);
+
+            int minNum = linkList.Head.Data;
+            linkList.ForEach(m => { if (m < minNum) minNum = m; });
+            Console.WriteLine("链表最小值为：" + minNum);
+
+            int sum = 0;
+            linkList.ForEach(m => sum += m);
+            Console.WriteLine("链表元素和为：" + sum);
+        }
+    }
+}
