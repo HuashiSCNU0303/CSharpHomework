a86a61b4c85fdc87e153dbec86e5955f8162e3a3
Ye_Junhao <HuashiSCNU0303@hotmail.com>
2020-04-26 09:15:23 +0800

    update Homework10

---
+++ Homework10/SimpleCrawler_WinForm_Parallel/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCrawler_WinForm
+{
+    public partial class Form1 : Form
+    {
+        SimpleCrawler myCrawler = new SimpleCrawler();
+        public Form1()
+        {
+            InitializeComponent();
+            simpleCrawlerBindingSource.DataSource = new BindingList<string>(myCrawler.successUrls);
+            simpleCrawlerBindingSource1.DataSource = new BindingList<string>(myCrawler.failureUrls);
+            
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            myCrawler.sendSuccessEvent += updateSuccessList;
+            myCrawler.sendFailureEvent += updateFailureList;
+            myCrawler.sendCrawlEndEvent += endCrawl;
+            myCrawler.sendCurrentUrlEvent += updateCurrentUrl;
+            myCrawler.startCrawl(txtStartSite.Text);
+        }
+
+        private void updateSuccessList()
+        {
+            if (lbxSuccessList.InvokeRequired)
+            {
+                Action sendInfo = new Action(updateSuccessList);
+                lbxSuccessList.Invoke(sendInfo, new object[] {});
+            }
+            else
+            {
+                simpleCrawlerBindingSource.DataSource = new BindingList<string>(myCrawler.successUrls);
+                lbxSuccessList.SelectedIndex = lbxSuccessList.Items.Count - 1;
+            }
+        }
+        private void updateFailureList()
+        {
+            if (lbxFailureList.InvokeRequired)
+            {
+                Action sendInfo = new Action(updateFailureList);
+                lbxFailureList.Invoke(sendInfo, new object[] {});
+            }
+            else
+            {
+                simpleCrawlerBindingSource1.DataSource = new BindingList<string>(myCrawler.failureUrls);
+                lbxFailureList.SelectedIndex = lbxFailureList.Items.Count - 1;
+            }
+        }
+        private void endCrawl()
+        {
+            MessageBox.Show("爬虫已结束！");
+        }
+        private void updateCurrentUrl(string url)
+        {
+            if (lblCurrentSite.InvokeRequired)
+            {
+                Action<string> sendInfo = new Action<string>(updateCurrentUrl);
+                lblCurrentSite.Invoke(sendInfo, new object[] { url });
+            }
+            else
+            { 
+                lblCurrentSite.Text = url;
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            // myCrawler.stopCrawl();
+            MessageBox.Show("改为多线程后暂时无法停止，请等待本次爬虫完成！");
+        }
+    }
+}
---
+++ Homework10/SimpleCrawler_WinForm_Parallel/SimpleCrawler.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace SimpleCrawler_WinForm
+{
+    class SimpleCrawler
+    {
+        public event Action sendSuccessEvent, sendFailureEvent, sendCrawlEndEvent;
+        public event Action<string> sendCurrentUrlEvent;
+        private ConcurrentDictionary<string, bool> urlVisited;
+        private ConcurrentQueue<string> urls;
+        public List<string> successUrls, failureUrls;
+        private List<Task> tasks;
+        private int count = 0;
+        private int currentTasks;
+        private int maxTaskNum { get; set; }
+        private int maxPageNum { get; set; }
+        public SimpleCrawler()
+        {
+            urls = new ConcurrentQueue<string>();
+            successUrls = new List<string>();
+            failureUrls = new List<string>();
+            urlVisited = new ConcurrentDictionary<string, bool>();
+            maxTaskNum = 5;
+            maxPageNum = 30;
+        }
+
+        public void startCrawl(string startUrl)
+        {
+            urls = new ConcurrentQueue<string>();
+            count = 0;
+            successUrls.Clear();
+            sendSuccessEvent();
+            failureUrls.Clear();
+            sendFailureEvent();
+
+            urls.Enqueue(startUrl);
+            
+            urlVisited.Clear();
+            urlVisited[startUrl] = true;
+            
+            tasks = new List<Task>();
+            Task t1 = Task.Factory.StartNew(Crawl);
+            tasks.Add(t1);
+            currentTasks = 1;
+        }
+        private void nextStart()
+        {
+            lock (this)
+            {
+                if (tasks.Count < maxTaskNum && urls.Count > maxTaskNum - tasks.Count)
+                {
+                    var t2 = Task.Factory.StartNew(Crawl);
+                    tasks.Add(t2);
+                    currentTasks++;
+                }
+            }
+        }
+
+        private void Crawl()
+        {
+            while (true)
+            {
+                lock (this)
+                {
+                    if (count >= maxPageNum || urls.Count == 0) 
+                    {
+                        if (currentTasks == 1) 
+                        {
+                            sendCrawlEndEvent();
+                        }
+                        currentTasks--;
+                        break;
+                    }
+                }
+                string current;
+                urls.TryDequeue(out current);
+                sendCurrentUrlEvent(current);
+                string html = DownLoad(current); // 下载
+                
+                if (html == "") 
+                {
+                    continue;
+                }
+                Parse(html, current);//解析,并加入新的链接
+                successUrls.Add(current);
+                sendSuccessEvent();
+            }
+        }
+
+        public string DownLoad(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                
+                string html = webClient.DownloadString(url);
+                string fileName = count.ToString();
+                count++;
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception ex)
+            {
+                failureUrls.Add(url);
+                sendFailureEvent();
+                return "";
+            }
+        }
+
+        private void Parse(string html, string url)
+        {
+            string strRef = @"(href|HREF)[]*=[]*[""'][^""'#>]+[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                strRef = match.Value.Substring(match.Value.IndexOf('=') + 1)
+                          .Trim('"', '\"', '#', '>', '\'');
+                if (strRef.Length == 0 || strRef == "javascript:void(0);") continue;
+                string url_transformed = null;
+                if (strRef.Contains("http://") || strRef.Contains("https://"))
+                {
+                    url_transformed = strRef;
+                }
+                else
+                {
+                    if (strRef.Contains(".html") || strRef.Contains(".aspx"))
+                    {
+                        url_transformed = Transform(strRef, url);
+                    }
+                }
+
+                if (url_transformed != null && !urlVisited.ContainsKey(url_transformed)) 
+                {
+                    urls.Enqueue(url_transformed);
+                    nextStart();
+                    urlVisited.TryAdd(url_transformed, true);
+                }
+            }
+
+        }
+
+        private string Transform(string urlX, string objurl)
+        {
+            Uri baseUri = new Uri(objurl);
+            Uri absoluteUri = new Uri(baseUri, urlX);
+            return absoluteUri.ToString();
+        }
+    }
+}
+
